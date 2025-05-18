300281f9f9a2fd28c5aa15ddfbe617fb84cbf74e
Воробьев Антон <vorobevanton1285@gmail.com>
2025-05-18 21:40:59 +0300

    v1.0.1

--- SKO.m
+++ SKO.m
@@ -1,21 +1,69 @@
-cb13 = [1, 1, 1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1];
+addpath(fullfile(pwd, 'GenerationSignal'))
+addpath(fullfile(pwd, 'corr_and_conv'))
 
-sig = zeros(size(t));
-period_sample_count = floor(length(t)/c);
+%noiseSIG = add_awgn_noise(sig,-3)
+%[lags, corr_norm] = correlation(noiseSIG, timer, sig, timer)
+%[max_time] = find_max_with_time(lags,corr_norm)
+% Задание кодов Баркера, которые нужно обрабатывать
+codes = [2,3,4,5,7,11,13];
+numCodes = length(codes);
 
-for i = 1:c
-    start_idx = (i-1)*period_sample_count + 1;
-    end_idx = i*period_sample_count;
-    if end_idx > length(t)
-        end_idx = length(t);
+% Предварительно выделяем массив для результатов: строки - коды, столбцы - уровни шума
+MassGraph = zeros(numCodes, 100);
+
+for idx = 1:numCodes
+    c = codes(idx);
+    disp(['Обработка кода Баркера: ', num2str(c)]);
+    
+    [timer, sig] = generate_barker_code(c, 0, 1, 100, 0, 10000, 0, 1/100, 'rect');
+    SkoMass = zeros(1, 100);  % для каждого уровня шума
+    
+    for i = 1:100
+        selection = zeros(1, 1000);  % для 1000 запусков
+        
+        for j = 1:1000
+            noiseSIG = add_awgn_noise(sig, -1 * i);
+            [lags, corr_norm] = correlation(noiseSIG, timer, sig, timer);
+            max_time = find_max_with_time(lags, corr_norm);
+            selection(j) = max_time;
+        end
+        
+        % Среднее и стандартное отклонение для текущего уровня шума
+        % Среднее не используется, можно убрать, если не нужно
+        % average = mean(selection);
+        SkoMass(i) = std(selection);
     end
-    sig(start_idx:end_idx) = cb3(i);
+    
+    MassGraph(idx, :) = SkoMass;  % записываем результаты для данного кода
 end
-add_awgn_noise(sig,-3)
-figure('Position',[100 100 1200 600]);
-subplot(2,2,1);
-plot(t, sig, 'LineWidth', 1.5);
-title(['Код Баркера ', num2str(c)]);
-xlabel('Время (с)');
-ylabel('Амплитуда');
-grid on;
+
+% Построение графиков
+colors = {'b', 'r', 'g', 'm', 'k', 'c', [0.5 0.5 0]}; % цвета для графиков
+
+hAx = subplot(2,2,1);
+hold(hAx, 'on');
+
+hPlots = gobjects(1, numCodes); % массив для дескрипторов графиков
+
+for k = 1:numCodes
+    hPlots(k) = plot(1:100, MassGraph(k, :), 'Color', colors{k}, 'LineWidth', 1.5);
+end
+
+hold(hAx, 'off');
+
+legendStrings = arrayfun(@(x) ['К.Б. ', num2str(x)], codes, 'UniformOutput', false);
+legend(hAx, hPlots, legendStrings, 'Location', 'best');
+
+title(hAx, 'Защищенность кодов Баркера');
+xlabel(hAx, 'Уровень шума, -dB');
+ylabel(hAx, 'Уровень СКО');
+grid(hAx, 'on');
+
+% Увеличиваем область графика, изменяя позицию осей:
+pos = get(hAx, 'Position');
+pos(1) = pos(1) + 0.05;  % сдвинуть вправо
+pos(2) = pos(2) - 0.35;  % сдвинуть вниз
+pos(3) = pos(3) + 0.35;  % увеличить ширину
+pos(4) = pos(4) + 0.35;  % увеличить высоту
+set(hAx, 'Position', pos);
+
